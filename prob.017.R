19d6cddf9ee16ad98c904f8eb4705b0c8bf68cca
JiHong Kim <mittens@sk.com>
2019-09-25 15:38:19 +0900

    Complete Problem 16

---
+++ prob.017.R
@@ -0,0 +1,43 @@
+a<-c(3 # one
+,3 # two
+,5 # three
+,4 # four
+,4 # five
+,3 # six
+,5 # seven
+,5 # eight
+,4 # nine
+)
+
+b<-c(3 # ten
+,6 # eleven
+,6 # twelve
+,8 # thirteen
+,8 # fourteen
+,7 # fifteen
+,7 # sixteen
+,9 # seventeen
+,8 # eighteen
+,8 # nineteen
+)
+
+cc<-c(6 # twenty
+,6 # thirty
+,5 # forty
+,5 # fifty
+,5 # sixty
+,7 # seventy
+,6 # eighty
+,6 # ninety
+)
+
+sum(a) + sum(b) 
+  
+sum(cc*length(a))
+
+a*7
+
+7 # hundred
+8 # thousand
+
+and
